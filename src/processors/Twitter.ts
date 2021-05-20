8b2a4f8057bfaaa4ea8d454a36f001cf568898f1
4lch4 <git@4lch4.dev>
2021-05-20 18:45:50 -0500

    refactor: trying something else for reading/writing JSON

--- src/processors/Twitter.ts
+++ src/processors/Twitter.ts
@@ -1,13 +1,15 @@
 // Helper library written for useful postprocessing tasks with Flat Data
 // Has helper functions for manipulating csv, txt, json, excel, zip, and image files
 // You can test this script locally on your computer by runinng `deno run -A --unstable postprocess.ts data.json`
+import {
+  readJSON
+} from 'https://deno.land/x/flat@0.0.10/mod.ts'
 import * as log from 'https://deno.land/std@0.97.0/log/mod.ts'
-import { readJson, writeJson } from 'https://deno.land/x/jsonfile/mod.ts'
+import { writeJson } from 'https://deno.land/x/jsonfile/mod.ts'
 
 // Get the data filename as the first argument
 const filename = Deno.args[0]
-const data = await readJson(filename)
-log.debug(data)
+const data = await readJSON(filename)
 const createdAt = data[0].created_at
 const newFilename = `${filename.substring(
   0,
