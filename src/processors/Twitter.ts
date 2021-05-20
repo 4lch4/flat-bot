24e38965f409654d1d8e707587488555fe2a5a93
4lch4 <git@4lch4.dev>
2021-05-20 18:41:47 -0500

    refactor: replaced flat module for jsonfile

--- src/processors/Twitter.ts
+++ src/processors/Twitter.ts
@@ -1,15 +1,12 @@
 // Helper library written for useful postprocessing tasks with Flat Data
 // Has helper functions for manipulating csv, txt, json, excel, zip, and image files
 // You can test this script locally on your computer by runinng `deno run -A --unstable postprocess.ts data.json`
-import {
-  readJSON,
-  writeJSON
-} from 'https://deno.land/x/flat@0.0.10/mod.ts'
 import * as log from 'https://deno.land/std@0.97.0/log/mod.ts'
+import { readJson, writeJson } from 'https://deno.land/x/jsonfile/mod.ts'
 
 // Get the data filename as the first argument
 const filename = Deno.args[0]
-const data = await readJSON(filename)
+const data = await readJson(filename)
 const createdAt = data[0].created_at
 const newFilename = `${filename.substring(
   0,
@@ -18,4 +15,4 @@ const newFilename = `${filename.substring(
 
 log.debug(`newFilename = ${newFilename}`)
 
-await writeJSON(newFilename, data[0].trends)
+await writeJson(newFilename, data[0].trends, { spaces: 2 })
