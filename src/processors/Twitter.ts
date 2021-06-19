26494361b7a4a0e2264b1f3b30bc59031a56bc0c
4lch4 <git@4lch4.dev>
2021-06-19 06:20:48 -0500

    fix: twitter post-processor

--- src/processors/Twitter.ts
+++ src/processors/Twitter.ts
@@ -3,11 +3,10 @@
 // You can test this script locally on your computer by runinng `deno run -A --unstable postprocess.ts data.json`
 import { ensureDir } from 'https://deno.land/std@0.99.0/fs/ensure_dir.ts'
 import { join } from 'https://deno.land/std@0.99.0/node/path.ts'
+
 // Get the data filename as the first argument
 const filename = Deno.args[0]
-const data = await Deno.readTextFile(filename)
-
-console.log(JSON.stringify(data, undefined, 2))
+const data = JSON.parse(await Deno.readTextFile(filename))
 
 const createdAt = data[0].created_at
 const newFilename = `${filename.substring(
@@ -15,8 +14,8 @@ const newFilename = `${filename.substring(
   filename.indexOf('.json')
 )}-${createdAt.replace(':', '.')}.json`
 
-const outDir = join('data', 'osrs')
+const outDir = join('data', 'twitter')
 
 await ensureDir(outDir)
 
-Deno.writeTextFileSync(newFilename, data[0].trends)
+await Deno.writeTextFile(newFilename, data[0].trends)
