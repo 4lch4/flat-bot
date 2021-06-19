b495d4c64aea9354495bbbc1d5ea7e25aab4ebf1
4lch4 <git@4lch4.dev>
2021-06-19 04:26:08 -0500

    test: trying something different for reading/writing JSON

--- src/processors/OSRS.ts
+++ src/processors/OSRS.ts
@@ -2,17 +2,15 @@
 // Has helper functions for manipulating csv, txt, json, excel, zip, and image files
 // You can test this script locally on your computer by running `deno run -A --unstable postprocess.ts data.json`
 // import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.10/mod.ts'
-import {
-  readJSON, removeFile, writeJSON
-} from 'https://deno.land/x/flat@0.0.10/mod.ts'
+import { readJsonSync, writeJsonSync } from 'https://deno.land/std@0.39.0/fs/mod.ts'
 
 const filename = Deno.args[0]
-const data = await readJSON(filename)
+const data = await readJsonSync(filename)
 // const createdAt = data[0].created_at
 const newFilename = `${filename.substring(
   0,
   filename.indexOf('.json')
 )}-${new Date().toString().replace(':', '.').replace(' ', '.')}.json`
 
-await removeFile(filename)
-await writeJSON(newFilename, data)
+// await removeFile(filename)
+await writeJsonSync(newFilename, data)
