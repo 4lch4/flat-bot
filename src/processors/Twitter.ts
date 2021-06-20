31a843d2d576297d3c18a2d9168754abb2659e3f
4lch4 <git@4lch4.dev>
2021-06-20 12:40:37 -0500

    test: removed deferred

--- src/processors/Twitter.ts
+++ src/processors/Twitter.ts
@@ -1,10 +1,9 @@
 // Helper library written for useful postprocessing tasks with Flat Data
 // Has helper functions for manipulating csv, txt, json, excel, zip, and image files
 // You can test this script locally on your computer by runinng `deno run -A --unstable postprocess.ts data.json`
-import { deferred } from 'https://deno.land/std/async/mod.ts'
 import { getNewFilename, readJSON } from '../lib/Utils.ts'
 
-const main = async (): deferred<string> => {
+const main = async () => {
   try {
     const content = await readJSON(Deno.args[0])
     const filename = getNewFilename(Deno.args[0], 'twitter')
